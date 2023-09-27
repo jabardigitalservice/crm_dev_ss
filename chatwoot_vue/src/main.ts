5d7c70391c6da2dd11f1931a6c42cf50ce2fa97f
Sandi <sandisunandar99@gmail.com>
2023-09-27 18:27:29 +0700

    testing app

---
+++ chatwoot_vue/src/main.ts
@@ -0,0 +1,26 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createChatWoot } from '@productdevbook/chatwoot/vue'
+
+const chatwoot = createChatWoot({
+  init: {
+    baseUrl: "http://127.0.0.1:3000",
+    websiteToken: "iTcF7Wjc4FVPP4V5uyhBEEVz",
+  },
+  settings: {
+    locale: "en",
+    position: "right",
+    launcherTitle: "Tanya Kami!",
+  },
+  partytown: false,
+});
+
+
+const app = createApp(App)
+
+app.use(router)
+app.use(chatwoot)
+app.mount('#app')
